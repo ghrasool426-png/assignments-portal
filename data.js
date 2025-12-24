c9f3bdcaf0f41d044bc0e8fdddebefd50d4066af
ghrasool426-png <ghrasool426@gmail.com>
2025-12-24 09:42:07 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const assignments = [
+  {
+    programme: "MA English",
+    course: "MEG01",
+    session: "Jan",
+    file: "pdf/meg01.pdf"
+  },
+  {
+    programme: "MA Political Science",
+    course: "MPS01",
+    session: "July",
+    file: "pdf/mps01.pdf"
+  },
+  {
+    programme: "BA General",
+    course: "BAG01",
+    session: "Jan",
+    file: "pdf/bag01.pdf"
+  }
+];
