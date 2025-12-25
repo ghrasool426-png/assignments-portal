659fd18bc2ea4867caaa1d5c3434a37830a0057b
ghrasool426-png <ghrasool426@gmail.com>
2025-12-25 10:20:23 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,24 +1,7 @@
-
-const assignments = [
-
-  // ===== MA =====
-  { level:"MA", programme:"English", course:"MEG01", session:"Jan", file:"pdf/MA/English/MEG01.pdf" },
-  { level:"MA", programme:"English", course:"MEG02", session:"July", file:"pdf/MA/English/MEG02.pdf" },
-
-  { level:"MA", programme:"Political Science", course:"MPS01", session:"Jan", file:"pdf/MA/Political_Science/MPS01.pdf" },
-  { level:"MA", programme:"History", course:"MAH01", session:"July", file:"pdf/MA/History/MAH01.pdf" },
-
-  // ===== BA =====
-  { level:"BA", programme:"General", course:"BAG01", session:"Jan", file:"pdf/BA/General/BAG01.pdf" },
-  { level:"BA", programme:"Political Science", course:"BPSC01", session:"July", file:"pdf/BA/Political_Science/BPSC01.pdf" },
-
-  // ===== BED =====
-  { level:"BED", programme:"B.Ed", course:"BED01", session:"Jan", file:"pdf/BED/BED01.pdf" },
-
-  // ===== DIPLOMA =====
-  { level:"DIPLOMA", programme:"Education", course:"DELED01", session:"July", file:"pdf/DIPLOMA/Education/DELED01.pdf" },
-
-  // ===== CERTIFICATE =====
-  { level:"CERTIFICATE", programme:"IT", course:"CIT01", session:"Jan", file:"pdf/CERTIFICATE/IT/CIT01.pdf" }
-
-];
+{
+  level: "MA",
+  programme: "English",
+  course: "MEG01",
+  session: "Jan",
+  file: "pdf/MA/English/MEG01.pdf"
+}
