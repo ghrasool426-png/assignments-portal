017419a8a04f2090ba5ec8521918dd2fbc6f4795
ghrasool426-png <ghrasool426@gmail.com>
2025-12-25 10:01:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-const list = document.getElementById("assignmentList");
-const search = document.getElementById("search");
-const sessionFilter = document.getElementById("sessionFilter");
-
-function render(data) {
-  list.innerHTML = "";
-  data.forEach(item => {
-    list.innerHTML += `
-      <div class="card">
-        <b>${item.programme}</b><br>
-        Course: ${item.course} | Session: ${item.session}
-        <a class="btn" href="${item.file}" download>Download</a>
-      </div>
-    `;
-  });
-}
-
-render(assignments);
-
-function filterData() {
-  const text = search.value.toLowerCase();
-  const session = sessionFilter.value;
-
-  const filtered = assignments.filter(a =>
-    a.course.toLowerCase().includes(text) &&
-    (session === "all" || a.session === session)
-  );
-
-  render(filtered);
-}
-
-search.addEventListener("input", filterData);
-sessionFilter.addEventListener("change", filterData);
+<select id="levelFilter">
+  <option value="all">All Programmes</option>
+  <option value="MA">MA</option>
+  <option value="BA">BA</option>
+  <option value="BED">B.Ed</option>
+  <option value="DIPLOMA">Diploma</option>
+  <option value="CERTIFICATE">Certificate</option>
+</select>
