b8a3109ba1483bf895633b12e7e0d29e3b05627f
ghrasool426-png <ghrasool426@gmail.com>
2025-12-25 18:26:53 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,41 @@
-{
-  level: "MA",
-  programme: "English",
-  course: "MEG01",
-  session: "Jan",
-  file: "pdf/MA/English/MEG01.pdf"
-}
+level: "BSC"
+level: "MA"
+level: "BA"
+level: "DIPLOMA"
+const assignments = [
+  {
+    level: "MA",
+    programme: "English",
+    course: "MEG01",
+    session: "Jan",
+    file: "pdf/MA/English/MEG01.pdf"
+  },
+  {
+    level: "MA",
+    programme: "Political Science",
+    course: "MPS01",
+    session: "July",
+    file: "pdf/MA/Political_Science/MPS01.pdf"
+  },
+  {
+    level: "BA",
+    programme: "General",
+    course: "BAG01",
+    session: "Jan",
+    file: "pdf/BA/General/BAG01.pdf"
+  },
+  {
+    level: "BSC",
+    programme: "Science",
+    course: "BSC01",
+    session: "July",
+    file: "pdf/BSC/Science/BSC01.pdf"
+  },
+  {
+    level: "DIPLOMA",
+    programme: "Education",
+    course: "DELED01",
+    session: "Jan",
+    file: "pdf/DIPLOMA/Education/DELED01.pdf"
+  }
+];
