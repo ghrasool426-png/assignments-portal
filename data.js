c4c34615ce36a3da000eacc68a354317f5d5ce9a
ghrasool426-png <ghrasool426@gmail.com>
2025-12-26 15:34:33 +0530

    Update data.js

--- data.js
+++ data.js
@@ -6,98 +6,98 @@ const assignments = [
     programme: "English",
     course: "MEG01",
     session: "Jan",
-    file: "pdf/MA/English/MEG01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Hindi",
     course: "MHD01",
     session: "Jan",
-    file: "pdf/MA/Hindi/MHD01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Urdu",
     course: "MUD01",
     session: "Jan",
-    file: "pdf/MA/Urdu/MUD01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Political Science",
     course: "MPS01",
     session: "Jan",
-    file: "pdf/MA/Political_Science/MPS01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "History",
     course: "MAH01",
     session: "Jan",
-    file: "pdf/MA/History/MAH01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Sociology",
     course: "MSO01",
     session: "Jan",
-    file: "pdf/MA/Sociology/MSO01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Psychology",
     course: "MAPC01",
     session: "Jan",
-    file: "pdf/MA/Psychology/MAPC01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Economics",
     course: "MEC01",
     session: "Jan",
-    file: "pdf/MA/Economics/MEC01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Public Administration",
     course: "MPA01",
     session: "Jan",
-    file: "pdf/MA/Public_Administration/MPA01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Education",
     course: "MAEDU01",
     session: "Jan",
-    file: "pdf/MA/Education/MAEDU01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Philosophy",
     course: "MAPY01",
     session: "Jan",
-    file: "pdf/MA/Philosophy/MAPY01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Anthropology",
     course: "MANT01",
     session: "Jan",
-    file: "pdf/MA/Anthropology/MANT01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Rural Development",
     course: "MARD01",
     session: "Jan",
-    file: "pdf/MA/Rural_Development/MARD01.pdf"
+    file: "#"
   },
   {
     level: "MA",
     programme: "Journalism",
     course: "MAJMC01",
     session: "Jan",
-    file: "pdf/MA/Journalism/MAJMC01.pdf"
+    file: "#"
   },
 
   // ===== BA SUBJECT SEED =====
